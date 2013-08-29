9bd18d67c2d6096d1b5c69982555628731938ab3
Vikram Srinivas <iam.vikram.srinivas@gmail.com>
2013-08-29 16:52:41 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ias::Application.config.secret_token = '159027ac2ce18d31c5f5a031d6146dd3f43fb48e8e5cea593c73106a96627687539c37b947b45b116d87df05221890a9364a9807598455f2a05bb304c77f568e'
